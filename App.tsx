d1feb6fde780fc5ab8d3ecc84369bd533f8cf789
Mateus Pitura <mateuspitura@gmail.com>
2023-04-17 06:56:24 -0300

    test(navegacao): adicionado modulos de nabegacao
    Para poder navegar entre telas por meio do Tab Navigator, instalado dependencias e criado telas básicas iniciais

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native'; 
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Sobre from './Telas/Sobre';
+import Calculadora from './Telas/Calculadora';
+import Historico from './Telas/Historico';
 
-const fun = ()=>{
-  return "Pao"
-}
+const tabs = createBottomTabNavigator();
 
 export default function(){ 
-  return( 
-    <View>  
-      <Text>Mateus {fun()}</Text> 
-    </View> 
-  ); 
+  <NavigationContainer>
+    <tabs.Navigator>
+      <tabs.Screen
+        name='Sobre'
+        component={Sobre}
+        options={{title:'Sobre'}}
+      />
+      <tabs.Screen
+        name='Calculadora'
+        componente={Calculadora}
+        options={{title:'Calculadora'}}
+      />
+      <tabs.Screen
+        name='Historico'
+        componente={Historico}
+        options={{title:'Historico'}}
+      />
+    </tabs.Navigator>
+  </NavigationContainer>
 }; 
